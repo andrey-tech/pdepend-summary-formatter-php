bacc50ecbbd41889150239dc3048a57589151bf0
andrey-tech <andreynero598@gmail.com>
2026-03-29 06:56:53 +0300

    Add median Mi to ProjectMiMetrics

--- src/AndreyTech/Pdepend/Summary/Extractor/Parser/ProjectMiMetrics.php
+++ src/AndreyTech/Pdepend/Summary/Extractor/Parser/ProjectMiMetrics.php
@@ -2,7 +2,7 @@
 
 /**
  * @author    andrey-tech
- * @copyright 2023 andrey-tech
+ * @copyright 2023-2026 andrey-tech
  * @link      https://github.com/andrey-tech/
  * @license   MIT
  */
@@ -11,12 +11,22 @@ declare(strict_types=1);
 
 namespace AndreyTech\Pdepend\Summary\Extractor\Parser;
 
+use function array_reduce;
+use function array_sum;
+use function count;
+use function floor;
+use function max;
+use function min;
+use function sort;
+use function sqrt;
+
 final class ProjectMiMetrics
 {
     public float $max = 0.0;
     public float $min = 0.0;
     public float $avg = 0.0;
     public float $std = 0.0;
+    public float $median = 0.0;
     public int $total = 0;
 
     /**
@@ -31,14 +41,22 @@ final class ProjectMiMetrics
 
     public function calculate(): void
     {
-        if (empty($this->miList)) {
+        $this->total = count($this->miList);
+
+        if (0 === $this->total) {
             return;
         }
 
+        $this->calculateAverage();
+        $this->calculateMedian();
+
+        $this->miList = [];
+    }
+
+    public function calculateAverage(): void
+    {
         $this->min = min($this->miList);
         $this->max = max($this->miList);
-
-        $this->total = count($this->miList);
 
         $this->avg = array_sum($this->miList) / $this->total;
 
@@ -49,7 +67,16 @@ final class ProjectMiMetrics
         );
 
         $this->std = sqrt($variance / $this->total);
+    }
 
-        $this->miList = [];
+    private function calculateMedian(): void
+    {
+        sort($this->miList, SORT_NUMERIC);
+
+        $middle = floor($this->total / 2);
+
+        $this->median = ($this->total % 2) ?
+            $this->miList[$middle] :
+            ($this->miList[$middle - 1] + $this->miList[$middle]) / 2;
     }
 }
